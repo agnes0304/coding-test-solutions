abf4768e2ce9bcc6c8e4672488a7853df338bc03
agnes0304 <jiwoochoi0304@gmail.com>
2024-01-15 00:08:56 +0900

    04번 문제: ㄷ모양 만들기의 주석 추가

--- codingTest.js
+++ codingTest.js
@@ -17,11 +17,11 @@ function solution(arr) {
   let first = 0;
   let second = 0;
 
-  for (let i = 0; i < arr.length - 2; i++) {
-    first += arr[i];
+  for (let i = 0; i < arr.length - 2; i++) { // 최소한 3개의 부분 배열이 있어야 하므로 arr.length - 2
+    first += arr[i]; 
     if (first === part) {
-      second = 0;
-      for (let j = i + 1; j < arr.length - 1; j++) {
+      second = 0; // second 초기화
+      for (let j = i + 1; j < arr.length - 1; j++) { // 최소한 2개의 부분 배열이 있어야 하므로 arr.length - 1
         second += arr[j];
         if (second === part) {
           count++;
@@ -41,18 +41,23 @@ function solution(arr) {
 function findWidestCShape(cells) {
   let maxWidth = 0;
 
+  // 바깥 이중 for문: cells을 반복합니다. C 모양 시작점(startRow, startCol)을 정의.
+
   for (let startRow = 0; startRow < cells.length; startRow++) {
     for (let startCol = 0; startCol < cells[startRow].length; startCol++) {
       let char = cells[startRow][startCol];
 
-      for (let height = 2; startRow + height <= cells.length; height++) {
+      // 안쪽 for 문: 높이와 너비를 정의. 높이 최소 3, 너비 최소 2(문제에서 글케 줌)
+      for (let height = 3; startRow + height <= cells.length; height++) {
         for (
-          let width = 3;
+          let width = 2;
           startCol + width <= cells[startRow].length;
           width++
         ) {
+          // C 모양이 맞는지 체크
           if (isCShape(cells, startRow, startCol, width, height, char)) {
             let currentWidth = 2 * height + (width - 2);
+            // 너비를 계산하여 현재 maxWidth와 비교
             maxWidth = Math.max(maxWidth, currentWidth);
           }
         }
@@ -64,15 +69,16 @@ function findWidestCShape(cells) {
 }
 
 function isCShape(cells, startRow, startCol, width, height, char) {
+  // 위쪽과 아래쪽 row 확인: char이랑 같은지 확인 없으면 false
   for (let col = startCol; col < startCol + width; col++) {
     if (
-      cells[startRow][col] !== char ||
-      cells[startRow + height - 1][col] !== char
+      cells[startRow][col] !== char || // 위쪽
+      cells[startRow + height - 1][col] !== char // 아래쪽
     ) {
       return false;
     }
   }
-
+  // 이어주는 변 확인
   for (let row = startRow + 1; row < startRow + height - 1; row++) {
     if (cells[row][startCol] !== char) {
       return false;

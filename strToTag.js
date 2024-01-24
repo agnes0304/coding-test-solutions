d52b54ec368a3adfc3bb0c5a205fb08823fb7ef7
agnes0304 <jiwoochoi0304@gmail.com>
2024-01-24 18:13:30 +0900

    ongoing

--- strToTag.js
+++ strToTag.js
@@ -1,36 +1,39 @@
 const str = "front<p>blahblah</p><p>something<u>here</u>hello</p>back";
 /*
-OUTPUT: 
+INPUT: html string
+sample: "front<p>blahblah</p><p>something<u>here</u>hello</p>back"
 
-[
-	"front",
-	"p":"blahblah",
-	"p": [
-		"something",
-		{"u":"here"},
-		"hello"
-	]
-	"back"
-]
+OUTPUT: json object
+sample: ["front","p":"blahblah","p":["something",{"u":"here"},"hello"],"back"]
 */
 
-// 태그 없는 텍스트 잘라내기
-// 태그 있는 텍스트
-// < 만나면
-
 function htmlToJson(str) {
-  const result = [];
-  let start = 0;
-  while (start < str.length) {
-    if (str[start] !== "<") {
-      let textEndIdx = str.indexOf("<", start);
-      if (textEndIdx !== -1) {
-        result.push(str.slice(start, textEndIdx));
+  let result = [];
+  let idx = 0;
+
+  while (idx < str.length) {
+    if (str[idx] === "<") {
+      let tagClose = str.indexOf(">", idx);
+      let tagName = str[idx + 1];
+      let closeTag = str.indexOf(`</${tagName}>`, idx);
+      let tagContent = str.slice(tagClose + 1, closeTag);
+      let obj = {};
+      obj[tagName] = tagContent;
+      result.push(obj);
+      idx = closeTag;
+    } else {
+      let textEndIdx = str.indexOf("<", idx);
+      if (textEndIdx === -1) {
+        result.push(str.slice(idx));
+        idx = str.length;
       } else {
-        result.push(str.slice(start, str.length));
+        result.push(str.slice(idx, textEndIdx));
+        idx = textEndIdx;
       }
     }
   }
+
+  return JSON.stringify(result);
 }
 
 console.log(htmlToJson(str));
